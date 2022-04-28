77e49a3d2ac05945dfa7776c2d64b0a3f8b58244
jimhaines37 <jamesrhaines@icloud.com>
2022-04-28 17:48:43 -0500

    Final

--- answers.md
+++ answers.md
@@ -12,13 +12,58 @@ Place all written answers from `assignment-05.md` here for easier grading.
 
 - **1a.**
 
+To do this task, we would first select the coin with the largest value that is less than N. To find the largest coin less than N mathematically, we can use: biggestCoin = 2^floor(log_2(N)), because coins are worth 2^k and we use floor to make sure we have an integer and that our coin's value doesnt exceed N. We would then take the the value of this coin and subtract it from N, shown: N - coinVal. We will then repeat the process with this difference as our new N, continuosly decreaseing N until it reaches 0, this way we will have the smallest amount of coins used. We can create a recursive algorithm with this functionality.
+
+In psuedo code:
+
+```
+numCoins = 0
+
+foo(N, numCoins)
+    if N = 0, return numCoins
+
+    biggestCoin = 2^floor(log_2(N))
+    newN = N - biggestCoin
+    numCoins += 1
+
+    return foo(newN, numCoins)
+```
+
+- **1b.**
+
+Work: W(n) = W(n^logn) + 1
+
+Span: S(N) = S(logn) + 1
 
 
 
 
+- **2a.**
+
+Assume denominations: 1, 6, 8, 10 and assume N = 14
+
+Greedy algorithm will first take 10, because it is the largest value that is less than N. Now, it will take 1 four more times so that N will equal 14 using 5 coins: 10, 1, 1, 1, 1. It cannot take 6 or 8 after 10 because both of these values would cause the total to exceed N.
+
+The actual optimal solution is 2 coins: 6 and 8 so that N = 14. 
+
+Greedy solution will not consider all possible combinations, it will always take the largest value first which may exclude better solutions. We should instead look at all possible solutions first and select the best one. Greedy works well with powers of 2 but not necessarily otherwise.
 
 
 - **2b.**
 
+If we use dynamic programming to solve this problem, we should find all possible combinations of coins that equal N and then select the solution which requires the least amount of coins. We could store each combination in a tuple that contains a list of coin values and the number of coins used, like: ([10, 1, 1, 1, 1], 5), ([8, 6], 2), and then select the tuple whos second value is the smallest.
 
+W(n) = W(n*m)
+S(n) = S(n)
+
+
+
+- **3a.**
+
+Optimal substructure property for MED is:
+
+{MED(S[1:], T[1:]), if S[0] = T[0]
+1 + min(MED(S, T[1:]), MED(S[1:], T[1:]), MED(S[1:], T)), otherwise}
+
+where each MED function represents an insertion, a substitution, and a deletion, respectively
 
--- main.py
+++ main.py
@@ -7,23 +7,84 @@ alignments = [('book', 'back'), ('kookaburra', 'kookybird-'), ('relev-ant','-ele
 def MED(S, T):
     # TO DO - modify to account for insertions, deletions and substitutions
     if (S == ""):
-        return(len(T))
+      return(len(T))
     elif (T == ""):
-        return(len(S))
+      return(len(S))
     else:
-        if (S[0] == T[0]):
-            return(MED(S[1:], T[1:]))
-        else:
-            return(1 + min(MED(S, T[1:]), MED(S[1:], T)))
+      if (S[0] == T[0]):
+        return(MED(S[1:], T[1:]))
+      else:
+        return 1 + min(MED(S, T[1:]), MED(S[1:], T[1:]), MED(S[1:], T)) # insert, substitute, delete
+
 
 
 def fast_MED(S, T, MED={}):
-    # TODO -  implement memoization
-    pass
+    r, c = (len(S), len(T))
+    MED = [[0 for x in range(c+1)] for y in range(r+1)]
+
+    for i in range(len(S)+1):
+      for j in range(len(T)+1):
+        if i == 0:
+          MED[i][j] = j
+        elif j == 0:
+          MED[i][j] = i
+        elif S[i-1] == T[j-1]:
+          MED[i][j] = MED[i-1][j-1] 
+        else:
+          MED[i][j] = 1 + min(MED[i][j-1], MED[i-1][j-1], MED[i-1][j])
+        
+    return MED[-1][-1]
+
+
 
 def fast_align_MED(S, T, MED={}):
-    # TODO - keep track of alignment
-    pass
+    r, c = (len(S), len(T))
+    MED = [[0 for x in range(c)] for y in range(r)]
+
+    for i in range(len(S)):
+      for j in range(len(T)):
+        if i == 0:
+          MED[i][j] = j
+        elif j == 0:
+          MED[i][j] = i
+        elif S[i-1] == T[j-1]:
+          MED[i][j] = MED[i-1][j-1] 
+        else:
+          MED[i][j] = 1 + min(MED[i][j-1], MED[i-1][j-1], MED[i-1][j])
+
+    alignedS = ""
+    alignedT = "" 
+
+    if len(S) == 0:
+      alignedS = '-' * len(T)
+      return alignedS, T
+
+    if len(T) == 0:
+      alignedT = '-' * len(S)
+      return S, alignedT
+
+    if S[0] == T[0]:
+      alignedS = (S[0]+ fast_align_MED(S[1:], T[1:])[0])
+      alignedT = (T[0]+ fast_align_MED(S[1:], T[1:])[1])
+
+    insert = fast_MED(S, T[1:])
+    delete = fast_MED(S[1:], T)
+    sub = fast_MED(S[1:], T[1:])
+
+    if min(insert, delete, sub) == insert and insert != sub: 
+      alignedS = ('-' + fast_align_MED(S, T[1:])[0])
+      alignedT = (T[0] + fast_align_MED(S, T[1:])[1])
+    
+    if min(insert, delete, sub) == delete and delete != sub:
+      alignedS = (S[0] + fast_align_MED(S[1:], T)[0])
+      alignedT = ('-' + fast_align_MED(S[1:], T)[1])
+
+    if min(insert, delete, sub) == sub: 
+      alignedS = (S[0] + fast_align_MED(S[1:], T[1:])[0])
+      alignedT = (T[0] + fast_align_MED(S[1:], T[1:])[1])
+
+
+    return alignedS, alignedT
 
 def test_MED():
     for S, T in test_cases:
@@ -34,3 +95,7 @@ def test_align():
         S, T = test_cases[i]
         align_S, align_T = fast_align_MED(S, T)
         assert (align_S == alignments[i][0] and align_T == alignments[i][1])
+
+print(MED('elephant', 'relevant'))
+print(fast_MED('elephant', 'relevant'))
+print(fast_align_MED('elephant', 'relevant'))
